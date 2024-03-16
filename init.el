5131b37686e3a1c3bd2e2cfbeaf16cdc731ff959
Ziqin Gong <gongzq0301@gmail.com>
2024-03-16 13:02:08 +0800

    After installing Emacs+

--- init.el
+++ init.el
@@ -680,6 +680,7 @@
   (setq markdown-command "multimarkdown"))
 
 
+
 ;; latex
 ;; use AUCTeX
 (defun zq/org-latex-preview-scale-fn ()
@@ -887,8 +888,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(which-key vertico modus-themes marginalia gcmh exec-path-from-shell diminish counsel corfu cape auctex ace-window)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
