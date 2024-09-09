a4bb5e19ac7c1f4556c6890680de27e34f18eeb5
Ziqin Gong <gongzq0301@gmail.com>
2024-09-09 16:54:05 +0800

    latex commands for org-mode

--- init.el
+++ init.el
@@ -897,7 +897,9 @@
                                 "~/OneDrive/org-life/agenda.org")))
   :bind (("C-c a" . org-agenda)
          :map org-mode-map
-         ("C-c M-L" . org-store-link))
+         ("C-c M-L" . org-store-link)
+         ("C-c t C-m" . TeX-insert-macro)
+         ("C-c t C-e" . LaTeX-environment))
   :hook
   (org-mode . zq/prettify-org))
 
