243fdb78098167eb05cb98dfcc1393257d87d27c
Ziqin Gong <gongzq0301@gmail.com>
2025-09-05 17:17:10 +0800

    HiDPI on Linux breaks svg-lib, so I have to scale down :height manually

--- init.el
+++ init.el
@@ -304,6 +304,8 @@ respect these settings."
   (corfu-popupinfo-mode))
 
 ;; pretty icons for corfu
+;; Unusually large icons on HiDPI Ubuntu:
+;; https://github.com/jdtsmith/kind-icon/issues/46#issuecomment-2954493820
 (use-package kind-icon
   :ensure t
   :after corfu)
@@ -325,6 +327,7 @@ respect these settings."
 
 (add-hook 'after-make-frame-functions #'zq/frame-setup)
 (add-hook 'elpaca-after-init-hook #'zq/frames-setup)
+
 
 ;;;========================
 ;;; minibuffer completion
