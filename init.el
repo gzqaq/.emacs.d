345f95c2dab83a29641d42b8772dc9b8fbd9f437
Ziqin Gong <gongzq0301@gmail.com>
2026-01-19 17:34:26 +0800

    treesit version compatible for combobulate

--- init.el
+++ init.el
@@ -238,7 +238,7 @@ respect these settings."
   ;; scope focus for python
   (indent-bars-treesit-scope '((python function_definition class_definition for_statement
                                        if_statement with_statement while_statement)))
-  :hook ((python-ts-mode yaml-mode) . indent-bars-mode))
+  :hook ((python-ts-mode yaml-ts-mode) . indent-bars-mode))
 
 (defun zq/org-simple-python-mode ()
   "Inhibit hooks from running for `org' src blocks for Python."
@@ -711,32 +711,54 @@ https://lambdaland.org/posts/2024-08-19_fancy_eshell_prompt/#eshell-prompt."
 (use-package treesit
   :ensure nil
   :init
-  (setq treesit-language-source-alist
-        '((bash . ("https://github.com/tree-sitter/tree-sitter-bash"))
-          (c . ("https://github.com/tree-sitter/tree-sitter-c"))
-          (cpp . ("https://github.com/tree-sitter/tree-sitter-cpp"))
-          (cmake . ("https://github.com/uyha/tree-sitter-cmake"))
-          (dockerfile . ("https://github.com/camdencheek/tree-sitter-dockerfile"))
-          (json . ("https://github.com/tree-sitter/tree-sitter-json"))
-          (make . ("https://github.com/alemuller/tree-sitter-make"))
-          (python . ("https://github.com/tree-sitter/tree-sitter-python"))
-          ;; (ruby . ("https://github.com/tree-sitter/tree-sitter-ruby"))
-          (rust . ("https://github.com/tree-sitter/tree-sitter-rust"))
-          (toml . ("https://github.com/tree-sitter/tree-sitter-toml"))))
+  (defun mp-setup-install-grammars ()
+    "Install Tree-sitter grammars if they are absent."
+    (interactive)
+    (dolist (grammar
+             '((bash . ("https://github.com/tree-sitter/tree-sitter-bash"))
+               (c . ("https://github.com/tree-sitter/tree-sitter-c"))
+               (cpp . ("https://github.com/tree-sitter/tree-sitter-cpp"))
+               (cmake . ("https://github.com/uyha/tree-sitter-cmake"))
+               (dockerfile . ("https://github.com/camdencheek/tree-sitter-dockerfile"))
+               (make . ("https://github.com/alemuller/tree-sitter-make"))
+               ;; These are the versions that are known to work with Combobulate *and* Emacs.
+               (css . ("https://github.com/tree-sitter/tree-sitter-css" "v0.20.0"))
+               (go . ("https://github.com/tree-sitter/tree-sitter-go" "v0.20.0"))
+               (html . ("https://github.com/tree-sitter/tree-sitter-html" "v0.20.1"))
+               (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript" "v0.20.1" "src"))
+               (json . ("https://github.com/tree-sitter/tree-sitter-json" "v0.20.2"))
+               (python . ("https://github.com/tree-sitter/tree-sitter-python" "v0.20.4"))
+               (rust . ("https://github.com/tree-sitter/tree-sitter-rust" "v0.21.2"))
+               (toml . ("https://github.com/tree-sitter/tree-sitter-toml" "v0.5.1"))
+               (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "typescript/src"))
+               (yaml . ("https://github.com/ikatyang/tree-sitter-yaml" "v0.5.0"))))
+      (add-to-list 'treesit-language-source-alist grammar)
+      ;; Only install `grammar' if we don't already have it installed.
+      (unless (treesit-language-available-p (car grammar))
+        (treesit-install-language-grammar (car grammar)))))
   (dolist (mapping '((python-mode . python-ts-mode)
                      (sh-mode . bash-ts-mode)
-                     (rust-mode . rust-ts-mode)))
-    (add-to-list 'major-mode-remap-alist mapping)))
+                     (rust-mode . rust-ts-mode)
+                     (yaml-mode . yaml-ts-mode)
+                     (css-mode . css-ts-mode)
+                     (js2-mode . js-ts-mode)
+                     (typescript-mode . typescript-ts-mode)
+                     (bash-mode . bash-ts-mode)
+                     (conf-toml-mode . toml-ts-mode)
+                     (go-mode . go-ts-mode)
+                     (css-mode . css-ts-mode)
+                     (json-mode . json-ts-mode)
+                     (js-json-mode . json-ts-mode)))
+    (add-to-list 'major-mode-remap-alist mapping))
+  :config
+  (mp-setup-install-grammars))
 
 (use-package combobulate
   :ensure (:host github :repo "mickeynp/combobulate"
                  :depth 1 :main "combobulate.el")
   :after treesit
-  :defer t
-  :init
-  (setq combobulate-key-prefix "C-c o")
-  :hook
-  (python-ts-mode . combobulate-mode))
+  :custom (combobulate-key-prefix "C-c o")
+  :hook (prog-mode . combobulate-mode))
 
 ;; eglot
 (use-package eglot
